61022255e1b2873482bf3857076c0d615e79e229
Tejas Badjate <tejas.badjate@ibm.com>
2025-03-04 12:07:19 +0530

    splitted pypi packages installations

--- g/gensim/gensim_4.3.3_ubi_9.5.sh
+++ g/gensim/gensim_4.3.3_ubi_9.5.sh
@@ -36,15 +36,15 @@ pip3.12 --version
 
 # Upgrade pip and install required dependencies
 python3.12 -m pip install --upgrade pip 
-echo "installing wheel meson pytest requests ruamel-yaml.."
+echo "installing wheel meson pytest requests ruamel-yaml..."
 python3.12 -m pip install wheel meson pytest requests ruamel-yaml 
-echo "installing nbformat testfixtures mock nbconvert.."
+echo "installing nbformat testfixtures mock nbconvert..."
 python3.12 -m pip install nbformat testfixtures mock nbconvert
-echo "installing numpy.."
+echo "installing numpy..."
 python3.12 -m pip install numpy==1.26.4 
-echo "installing scipy.."
+echo "installing scipy..."
 python3.12 -m pip install scipy==1.13.1
-echo "installing cython.."
+echo "installing cython..."
 python3.12 -m pip install Cython
 # Clone the repository
 git clone $PACKAGE_URL $PACKAGE_DIR
